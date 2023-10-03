408c66e8d76da7e61708318246af7fa15ea103db
Alec Tuchscherer <alec@onesourceimaging.com>
2023-10-02 19:55:46 -0500

    Make it HTTP

--- index.py
+++ index.py
@@ -1,7 +1,15 @@
 from requests import get
-loc = get('https://ipapi.co/json/')
-res = loc.json()
-if ('error' in res):
-    print("The API has Rate Limited")
-else:
-    print ("Your IP is: " + loc.json()['ip'])
+from http.server import BaseHTTPRequestHandler
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        loc = get('https://ipapi.co/json/')
+        res = loc.json()
+        if ('error' in res):
+            out = "The API has Rate Limited"
+        else:
+            out = "Your IP is: " + loc.json()['ip']
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write(out.encode('utf-8'))
+        return
