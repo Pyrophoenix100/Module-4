fd931ccc6711bb5e5045c45e9dc5864d5fb3dd76
Alec Tuchscherer <alec@onesourceimaging.com>
2023-10-02 20:22:37 -0500

    API

--- api/index.py
+++ api/index.py
@@ -9,7 +9,7 @@ class handler(BaseHTTPRequestHandler):
         else:
             out = "Your IP is: " + loc.json()['ip']
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type','text/html')
         self.end_headers()
         self.wfile.write(out.encode('utf-8'))
         return
