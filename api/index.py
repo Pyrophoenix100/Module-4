465cf02f4e2514ec4a0fd429255e400a67bda44c
Pyro100 <alectuc@gmail.com>
2023-10-18 20:41:45 -0500

    Updated to Flask

--- api/index.py
+++ api/index.py
@@ -1,15 +1,12 @@
-from requests import get
-from http.server import BaseHTTPRequestHandler
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        loc = get('https://ipapi.co/json/')
-        res = loc.json()
-        if ('error' in res):
-            out = "The API has Rate Limited"
-        else:
-            out = "Your IP is: " + loc.json()['ip']
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-        self.wfile.write(out.encode('utf-8'))
-        return out
+import flask
+import requests
+app = flask.Flask(__name__)
+@app.route("/")
+def getIP():
+    loc = requests.get('https://ipapi.co/json/')
+    res = loc.json()
+    if ('error' in res):
+        out = "The API has Rate Limited"
+    else:
+        out = "Your IP is: " + loc.json()['ip']
+    return out
