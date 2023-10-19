66d3ac08d7eab42a4c69f13d9660f988b6d08d0c
Pyro100 <alectuc@gmail.com>
2023-10-18 21:36:21 -0500

    Error Handling

--- api/index.py
+++ api/index.py
@@ -1,23 +1,25 @@
 import flask
 import requests
 app = flask.Flask(__name__)
-@app.route("/api/ip")
-def main():
-    loc = requests.get('https://ipapi.co/json/')
-    res = loc.json()
-    if ('error' in res):
-        out = "The API has Rate Limited you"
+@app.route("/api/<path:path>")
+def main(path):
+    if (path == "ip"):
+        loc = requests.get('https://ipapi.co/json/')
+        res = loc.json()
+        if ('error' in res):
+            out = "The API has Rate Limited you"
+        else:
+            out = "Your IP is: " + loc.json()['ip']
+        return out
+    elif (path == "loc"):
+        loc = requests.get('https://ipapi.co/json/')
+        res = loc.json()
+        if ('error' in res):
+            out = "The API has Rate Limited you"
+        else:
+            out = "Your location is " + res['city'] + ", " + res['region'] + " " + res['country']
+        return out
     else:
-        out = "Your IP is: " + loc.json()['ip']
-    return out
-@app.route("/api/location")
-def location():
-    loc = requests.get('https://ipapi.co/json/')
-    res = loc.json()
-    if ('error' in res):
-        out = "The API has Rate Limited you"
-    else:
-        out = "Your location is " + res['city'] + ", " + res['region'] + " " + res['country']
-    return out
+        return path + " was not found on the server."
 if __name__ == '__main__':
     app.run(debug=True, port=8002)
