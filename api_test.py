48bcd48580a298b25a412c9d9fd71ad3cbd03c4d
Pyro100 <alectuc@gmail.com>
2023-10-18 23:31:13 -0500

    Changed Test Case It occurs to me that changing test cases might not be the solution to the problem. Shouldn't the test cases be the metric for the code? Not the other way around.

--- api_test.py
+++ api_test.py
@@ -2,4 +2,4 @@ import requests
 def test_ip():
     assert "Your IP is:" in requests.get("http://module-4-silk.vercel.app/api/ip").text
 def test_loc():
-    assert "Your location is:" in requests.get("http://module-4-silk.vercel.app/api/loc").text
+    assert "Your location is " in requests.get("http://module-4-silk.vercel.app/api/loc").text
